0c28ebc83df402a7900d40ac3de441bc2ed7dc43
Abenezer Tamene <t-abtamene@microsoft.com>
2022-08-05 11:51:05 -0700

    uncomment test file

--- codeql/windows-drivers/queries/PortedQueries/build_create_analyze_test.cmd
+++ codeql/windows-drivers/queries/PortedQueries/build_create_analyze_test.cmd
@@ -1,16 +1,16 @@
 @echo off
 
-@REM call :test KeWaitLocal KMDFTestTemplate
-@REM call :test PendingStatusError WDMTestingTemplate
-@REM call :test DispatchMismatch WDMTestingTemplate
-@REM call :test DispatchAnnotationMissing WDMTestingTemplate
-@REM call :test NoPagedCode WDMTestingTemplate
-@REM call :test MultiplePagedCode WDMTestingTemplate
-@REM call :test NoPagingSegment WDMTestingTemplate
-@REM call :test IrqTooHigh WDMTestingTemplate
-@REM call :test IrqTooLow WDMTestingTemplate
+call :test KeWaitLocal KMDFTestTemplate
+call :test PendingStatusError WDMTestingTemplate
+call :test DispatchMismatch WDMTestingTemplate
+call :test DispatchAnnotationMissing WDMTestingTemplate
+call :test NoPagedCode WDMTestingTemplate
+call :test MultiplePagedCode WDMTestingTemplate
+call :test NoPagingSegment WDMTestingTemplate
+call :test IrqTooHigh WDMTestingTemplate
+call :test IrqTooLow WDMTestingTemplate
 call :test ExaminedValue WDMTestingTemplate
-@REM call :test StrSafe KMDFTestTemplate
+call :test StrSafe KMDFTestTemplate
 
 
 
@@ -29,8 +29,8 @@ msbuild /t:rebuild /p:platform=x64
 echo creating_database
 codeql database create -l=cpp -c "msbuild /p:Platform=x64 /t:rebuild" "C:\Users\t-abtamene\Desktop\TestDB\%1"
 
-@REM echo analysing_database
-@REM codeql database analyze "C:\Users\t-abtamene\Desktop\TestDB\%1" --format=sarifv2.1.0 --output="..\..\AnalysisFiles\Test Samples\%1" "..\..\queries\%1\%1.ql" 
+echo analysing_database
+codeql database analyze "C:\Users\t-abtamene\Desktop\TestDB\%1" --format=sarifv2.1.0 --output="..\..\AnalysisFiles\Test Samples\%1" "..\..\queries\%1\%1.ql" 
 
 cd ..\..
 echo %0 %1 }
